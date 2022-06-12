a0bea072784579eff684f138287eda5bf6b4ec50
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-13 04:24:15 +0700

    route

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 5000
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
-
+const userRoute = require("./routes/user")
 dotenv.config();
 
 mongoose
-    .connect(
-        process.env.MONGOS_URL
-    )
+    .connect(process.env.MONGOS_URL)
     .then(() => console.log("database mongoose connted success!"))
     .catch((err) => {
         console.log(err);
 });
+
+app.use(express.json());
+app.use("/user/test", userRoute);
+
+
 app.listen(process.env.PORT || port, () => {
     console.log("backend is running ")
 })
